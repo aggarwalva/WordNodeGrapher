ce048aa549bfd351ad1597ec57e3b3958b3044a5
aggarwalva <varunneilaggarwal@gmail.com>
2019-10-01 23:53:11 -0400

    Changed sizing of visual

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,14 @@ import org.graphstream.graph.EdgeRejectedException;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.IdAlreadyInUseException;
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.view.View;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.ViewerPipe;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
 
@@ -34,9 +40,16 @@ public class Main {
         ArrayList<WordNode> dataNodes = input.getNodes();
 
         graph.setAttribute("ui.stylesheet", "node { text-size:24;}");
+
+        Random random = new Random();
         for(int i = 0; i < dataNodes.size(); i++){
             graph.addNode(dataNodes.get(i).text);
-            graph.getNode(i).addAttribute("ui.style", " size:" + dataNodes.get(i).size + "px;");
+            int hue = random.nextInt(5) * 10 + 100;
+            float saturation = random.nextFloat();
+            float value = random.nextFloat();
+            Color c = Color.getHSBColor(hue,saturation,value);
+            graph.getNode(i).addAttribute("ui.style", " size:" + dataNodes.get(i).size + "px;" +
+                    "fill-color: rgb(" + c.getRed() + "," + c.getGreen() + "," + c.getBlue() +");");
             graph.getNode(i).addAttribute("ui.label",dataNodes.get(i).text);
         }
         graph.display();
@@ -44,19 +57,20 @@ public class Main {
 
     public static void generateConnectionGraph(){
         Graph graph = new SingleGraph("Trial 1");
+        graph.addAttribute("ui.stylesheet", "graph { size: 100px, 400px;}");
 
-        InputReader input = new InputReader("C:\\Users\\Varun\\Desktop\\IdeaProjects\\TextWordAnalyzer\\wordsOutput - Copy (2).txt");
+        InputReader input = new InputReader("C:\\Users\\Varun\\Desktop\\IdeaProjects\\TextWordAnalyzer\\sonnets words.txt");
         ArrayList<WordNode> dataNodes = input.getNodes();
-        input = new InputReader("C:\\Users\\Varun\\Desktop\\IdeaProjects\\TextWordAnalyzer\\output.txt");
+        input = new InputReader("C:\\Users\\Varun\\Desktop\\IdeaProjects\\TextWordAnalyzer\\sonnets connections.txt");
         ArrayList<ConnectionNode> connections = ConnectionNode.GenerateConnectionNodes(input.getNodes());
 
-        graph.setAttribute("ui.stylesheet", "node { text-size:24;}");
+        graph.addAttribute("ui.stylesheet", "node { text-size:24;}");
         for(int i = 0; i < dataNodes.size(); i++){
             graph.addNode(dataNodes.get(i).text);
-            graph.getNode(i).addAttribute("ui.style", " size:" + dataNodes.get(i).size/2 + "px;" +
+            graph.getNode(i).addAttribute("ui.style", " size:" + (dataNodes.get(i).size < 100 ? dataNodes.get(i).size : 100) + "px;" +
                     "fill-color: grey;");
             graph.getNode(i).addAttribute("ui.label", dataNodes.get(i).text);
-            graph.getNode(i).addAttribute("layout.weight", 2);
+            //graph.getNode(i).addAttribute("layout.weight", 1);
             System.out.println("Size: " + dataNodes.get(i).size);
         }
 
@@ -68,7 +82,7 @@ public class Main {
             for(String s : l2){
                 try{
                     graph.addEdge(Integer.toString(edgeCount), graph.getNode(dataNodes.get(i).text), graph.getNode(s));
-                    graph.getEdge(edgeCount).setAttribute("layout.weight", 10);
+                   graph.getEdge(edgeCount).setAttribute("layout.weight", 0.4);
                     edgeCount++;
                 } catch(IdAlreadyInUseException e){
                     //e.printStackTrace();
@@ -78,6 +92,16 @@ public class Main {
             }
         }
 
+        /*Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+        View view = viewer.addDefaultView(false);
+        JFrame frame = new JFrame("Visualizer");
+        ViewerPipe fromViewer = viewer.newViewerPipe();
+        fromViewer.addSink(graph);
+
+        frame.setSize(540,1080);
+        frame.add((Component) view);
+
+        frame.setVisible(true);*/
         graph.display();
     }
 }
